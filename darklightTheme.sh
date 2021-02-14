d7712ce2fd1fc8d279f877f5f2003f901aa84c07
Bryan Imbaquingo <bryan_3871@hotmail.com>
2021-02-14 11:52:56 -0500

    icons

--- darklightTheme.sh
+++ darklightTheme.sh
@@ -9,8 +9,10 @@ then
 	gsettings set org.gnome.desktop.interface gtk-theme 'WhiteSur-light'
 	gsettings set org.gnome.shell.extensions.user-theme name 'WhiteSur-light-alt'
 	gsettings set org.gnome.desktop.background picture-uri $ligth
+	gsettings set org.gnome.desktop.interface icon-theme 'korla-light'
 else
 	gsettings set org.gnome.desktop.interface gtk-theme 'WhiteSur-dark'
 	gsettings set org.gnome.shell.extensions.user-theme name 'WhiteSur-dark' 
 	gsettings set org.gnome.desktop.background picture-uri $dark
+	gsettings set org.gnome.desktop.interface icon-theme 'korla-pgrey'
 fi
